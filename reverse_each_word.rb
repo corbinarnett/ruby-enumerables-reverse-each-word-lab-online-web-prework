1e012e99a31f15f6f10b37f46964f23394709f80
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-15 03:53:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -23,7 +23,3 @@ end
 reverse_each_word('Hello there, and how are you?')
 #=> "olleH ,ereht dna woh era ?uoy"
 
-
-def reverse_each_word(string)
-  string.split.collect { |word| word.reverse  }.join(" ")
-end
