7f5199076dc62fbf0ab58a93bf40ccd4332649db
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-15 03:59:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,12 +3,12 @@ def reverse_each_word_with_each(string)
   # First solve it using .each Then write the same method using .collect to see the difference.
   # turn the string into an array
   # .reverse reverses an array
-  original_array = string.split(' ')
-  return_array = []
-  original_array.each do |string|
-    return_array << string.reverse
+  string = string.split(“”)
+  reversed = []
+  string.each {|i| reversed.unshift(i)}
+  
   end
-  return_array.join(' ')
+  reversed.join(“”)
 end
 
 def reverse_each_word(string)
